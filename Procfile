50d780ca7bebb9a42495aa0c463dc7beb51d19ac
somil2204 <2204.somil@gmail.com>
2021-08-11 22:28:25 +0530

    123

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: sh setup.sh && streamlit run straddle.py
+echo PORT $PORT
+streamlit run  --server.port $PORT straddle.py
--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 mkdir -p ~/.streamlit/
+  
+echo "\
+[general]\n\
+email = \"qemail@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
+enableCORS=false\n\
 port = $PORT\n\
-enableCORS = false\n\
-\n\
 " > ~/.streamlit/config.toml
